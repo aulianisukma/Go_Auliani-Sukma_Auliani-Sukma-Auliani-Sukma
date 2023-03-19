eae55faaccdd853f1e49dbd53e835affdfd8936b
aulianisukma <aulianisukma10@gmail.com>
2023-03-19 11:42:54 +0700

    update query select

--- 16_JoinUnionAgregasiSubqueryFunction (DBMS)/Praktikum/soalselect.sql
+++ 16_JoinUnionAgregasiSubqueryFunction (DBMS)/Praktikum/soalselect.sql
@@ -3,10 +3,10 @@ SELECT name FROM users WHERE gender = 'M'
 --b.
 SELECT * FROM product WHERE id = '3'
 --c.
-SELECT * FROM user WHERE created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW() AND name LIKE '%a%';
+SELECT * FROM user WHERE '2023-03-18' BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW() AND 'Kai' LIKE '%a%';
 --d.
 SELECT COUNT(*) FROM user WHERE gender = 'F';
 --e.
 SELECT * FROM product LIMIT 5;
 --f.
-SELECT * FROM user ORDER BY name ASC;
+SELECT * FROM user ORDER BY 'Baekhyun' ASC;
